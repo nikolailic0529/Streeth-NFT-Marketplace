0f250c5ef1fabca83d06bdcd783e9c134543d0cd
Nikola Ilic <nikolaillic0529@gmail.com>
2023-05-17 12:39:31 -0400

    update react player

--- src/containers/PageHome/SectionVideos.tsx
+++ src/containers/PageHome/SectionVideos.tsx
@@ -52,7 +52,7 @@ const SectionVideos: FC<SectionVideosProps> = ({
           //   <source src="https://streamable.com/46j8zo" type="video/mp4" />
           //   our browser does not support the video tag.
           // </video>
-          <ReactPlayer url="https://streamable.com/46j8zo" controls height="100%" width="100%" />
+          <ReactPlayer url="https://streamable.com/46j8zo" muted={true} loop={true} height="100%" width="100%" />
         ) : (
           <>
             <div
